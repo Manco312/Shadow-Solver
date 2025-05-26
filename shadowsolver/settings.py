390e5c4f0906df7e0fa29ad03da6fe0638c2f300
Santiago <santiagomancomaya@gmail.com>
2025-05-26 15:29:31 -0500

    ADD: Prod files

--- shadowsolver/settings.py
+++ shadowsolver/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Whitenoise for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'shadowsolver/static/',
 ]
+
+# Producción: dónde recolectar archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilesprod')
+
+# Para servir archivos estáticos en producción
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
